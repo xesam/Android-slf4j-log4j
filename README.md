6f711a4112cdd548fc44bde5caf6e00f9fc7595b
xesam <xesamguo@gmail.com>
2016-06-18 17:13:24 +0800

    log

---
+++ README.md
@@ -0,0 +1,24 @@
+# Android Log
+
+1. Android + slf4j --> slf4j-api-xxx + slf4j-android-xxx
+2. Android + log4j --> log4j-xxx + android-logging-log4j
+3. Android + slf4j + log4j --> slf4j-api-xxx + slf4j-log4j-xxx.jar + android-logging-log4j
+
+## slf4j-api-xxx + slf4j-log4j-xxx + slf4j-android-xxx
+
+[slf4j-api](http://www.slf4j.org/download.html)
+
+## slf4j-android-xxx
+
+[slf4j-android](http://www.slf4j.org/android/)
+
+## log4j-xxx
+
+[log4j](http://logging.apache.org/log4j/1.2/download.html)
+
+## android-logging-log4j
+
+[android-logging-log4j](https://github.com/eetac/android-logging-log4j)
+
+## remove log with ProGuard
+
--- app/src/main/java/dev/xesam/android/slf4j_log4j/MainActivity.java
+++ app/src/main/java/dev/xesam/android/slf4j_log4j/MainActivity.java
@@ -1,7 +1,8 @@
 package dev.xesam.android.slf4j_log4j;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +10,19 @@ import org.slf4j.LoggerFactory;
 public class MainActivity extends AppCompatActivity {
 
     Logger logger = LoggerFactory.getLogger(MainActivity.class);
+    org.apache.log4j.Logger logger1 = org.apache.log4j.Logger.getLogger(MainActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logger.info("My Application Created");
+        findViewById(R.id.info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//        logger.info("My Application Created");
+                logger1.info("My Application Created");
+            }
+        });
+
     }
 }
